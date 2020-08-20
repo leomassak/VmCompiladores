e06d483ec0dbef51d1743abeae38537f8ee73b2d
Leonardo Massak <33417793+leomassak@users.noreply.github.com>
2020-08-20 20:04:16 -0300

    First Vm file upload

---
+++ VmCompiladores/VmCompiladores/Form1.Designer.cs
@@ -0,0 +1,394 @@
+﻿namespace VmCompiladores
+{
+    partial class VmSimulador
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button continueButton;
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.indexColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.InstructionColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.firstPropColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.secondPropColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CommentColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel11 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inputWindow = new System.Windows.Forms.ListBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.stackContent = new System.Windows.Forms.RichTextBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.runButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileDialog = new System.Windows.Forms.OpenFileDialog();
+            continueButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.panel11.SuspendLayout();
+            this.panel10.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel8.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // continueButton
+            // 
+            continueButton.Location = new System.Drawing.Point(456, 738);
+            continueButton.Name = "continueButton";
+            continueButton.Size = new System.Drawing.Size(191, 46);
+            continueButton.TabIndex = 1;
+            continueButton.Text = "Continuar";
+            continueButton.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(33, 38);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1109, 682);
+            this.panel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.indexColumn,
+            this.InstructionColumn,
+            this.firstPropColumn,
+            this.secondPropColumn,
+            this.CommentColumn});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 58);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(909, 443);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.Text = "dataGridView1";
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // indexColumn
+            // 
+            this.indexColumn.FillWeight = 38.07109F;
+            this.indexColumn.HeaderText = "I";
+            this.indexColumn.Name = "indexColumn";
+            this.indexColumn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // InstructionColumn
+            // 
+            this.InstructionColumn.FillWeight = 73.70057F;
+            this.InstructionColumn.HeaderText = "Instruções";
+            this.InstructionColumn.Name = "InstructionColumn";
+            this.InstructionColumn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // firstPropColumn
+            // 
+            this.firstPropColumn.FillWeight = 73.70057F;
+            this.firstPropColumn.HeaderText = "Atributo #1";
+            this.firstPropColumn.Name = "firstPropColumn";
+            this.firstPropColumn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // secondPropColumn
+            // 
+            this.secondPropColumn.FillWeight = 73.70057F;
+            this.secondPropColumn.HeaderText = "Atributo #2";
+            this.secondPropColumn.Name = "secondPropColumn";
+            this.secondPropColumn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // CommentColumn
+            // 
+            this.CommentColumn.FillWeight = 240.8275F;
+            this.CommentColumn.HeaderText = "Comentário";
+            this.CommentColumn.Name = "CommentColumn";
+            this.CommentColumn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // label5
+            // 
+            this.label5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(0, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(909, 58);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Instruções a serem executadas pela VM";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel3.Controls.Add(this.panel11);
+            this.panel3.Controls.Add(this.panel10);
+            this.panel3.Controls.Add(this.panel7);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 501);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(909, 181);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel11
+            // 
+            this.panel11.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel11.Controls.Add(this.label4);
+            this.panel11.Controls.Add(this.listBox3);
+            this.panel11.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel11.Location = new System.Drawing.Point(629, 0);
+            this.panel11.Name = "panel11";
+            this.panel11.Size = new System.Drawing.Size(278, 177);
+            this.panel11.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(67, -1);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 35);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Break Points";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 15;
+            this.listBox3.Location = new System.Drawing.Point(23, 37);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(236, 124);
+            this.listBox3.TabIndex = 0;
+            // 
+            // panel10
+            // 
+            this.panel10.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel10.Controls.Add(this.label3);
+            this.panel10.Controls.Add(this.listBox2);
+            this.panel10.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel10.Location = new System.Drawing.Point(273, 0);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(356, 177);
+            this.panel10.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(100, -1);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 35);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Janela de Saída";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(18, 37);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(319, 124);
+            this.listBox2.TabIndex = 0;
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.panel8);
+            this.panel7.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel7.Location = new System.Drawing.Point(0, 0);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(273, 177);
+            this.panel7.TabIndex = 0;
+            // 
+            // panel8
+            // 
+            this.panel8.Controls.Add(this.label2);
+            this.panel8.Controls.Add(this.inputWindow);
+            this.panel8.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel8.Location = new System.Drawing.Point(0, 0);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(273, 177);
+            this.panel8.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(58, 1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 35);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Janela de Entrada";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // inputWindow
+            // 
+            this.inputWindow.FormattingEnabled = true;
+            this.inputWindow.ItemHeight = 15;
+            this.inputWindow.Location = new System.Drawing.Point(9, 39);
+            this.inputWindow.Name = "inputWindow";
+            this.inputWindow.Size = new System.Drawing.Size(248, 124);
+            this.inputWindow.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.stackContent);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(909, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 682);
+            this.panel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 56);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Conteúdo da Pilha";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // stackContent
+            // 
+            this.stackContent.Location = new System.Drawing.Point(-2, 59);
+            this.stackContent.Name = "stackContent";
+            this.stackContent.Size = new System.Drawing.Size(196, 619);
+            this.stackContent.TabIndex = 0;
+            this.stackContent.Text = "";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.menuStrip1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(1169, 32);
+            this.panel4.TabIndex = 2;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileButton,
+            this.runButton});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1169, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked_1);
+            // 
+            // fileButton
+            // 
+            this.fileButton.Name = "fileButton";
+            this.fileButton.Size = new System.Drawing.Size(66, 20);
+            this.fileButton.Text = "Arquivos";
+            // 
+            // runButton
+            // 
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(64, 20);
+            this.runButton.Text = "Executar";
+            // 
+            // fileDialog
+            // 
+            this.fileDialog.FileName = "fileDialog";
+            this.fileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.fileDialog_FileOk);
+            // 
+            // VmSimulador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(1169, 813);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(continueButton);
+            this.Controls.Add(this.panel1);
+            this.Name = "VmSimulador";
+            this.Text = "VmSimulador";
+            this.Load += new System.EventHandler(this.VmSimulador_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel11.ResumeLayout(false);
+            this.panel10.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
+            this.panel8.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel11;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RichTextBox stackContent;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox inputWindow;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileButton;
+        private System.Windows.Forms.ToolStripMenuItem runButton;
+        private System.Windows.Forms.OpenFileDialog fileDialog;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn indexColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn InstructionColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn firstPropColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn secondPropColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CommentColumn;
+    }
+}
+
---
+++ VmCompiladores/VmCompiladores/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VmCompiladores
+{
+    public partial class VmSimulador : Form
+    {
+        public VmSimulador()
+        {
+            InitializeComponent();
+        }
+
+        private void menuStrip2_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void VmSimulador_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked_1(object sender, ToolStripItemClickedEventArgs e)
+        {
+            var fileContent = string.Empty;
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                var filePath = fileDialog.FileName;
+
+                string content = File.ReadAllText(filePath, Encoding.UTF8);
+                stackContent.Text = content;
+
+                StreamReader file = new StreamReader(filePath);
+
+                string newline;
+                while ((newline = file.ReadLine()) != null)
+                {
+
+                    string[] values = newline.Split(' ');
+                    string[] row = new string[values.Length];
+
+                    for (int j = 0; j < values.Length; j++)
+                    {
+                        row[j] = values[j].Trim();
+                    }
+                    dataGridView1.Rows.Add(row);
+                }
+            }
+        }
+
+        private void fileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
