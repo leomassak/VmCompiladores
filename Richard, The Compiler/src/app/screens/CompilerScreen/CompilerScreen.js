6bcd6125df7e7f52864f94735b4232751c606dca
Giovani Pinke <giovani.pinke@mblabs.com.br>
2020-08-20 22:19:05 -0300

    removing files

---
+++ Richard, The Compiler/src/app/screens/CompilerScreen/CompilerScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles.scss';
+
+class WelcomeScreen extends React.PureComponent {
+  render() {
+    return (
+      <div className="nome">
+        <h1>Bem vindo!</h1>
+      </div>
+    );
+  }
+}
+
+export default WelcomeScreen;
