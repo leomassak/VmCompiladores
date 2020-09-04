39964868a373c8ab282ce6ebf571b60c64e5122d
Giovani Pinke <giovani.pinke@mblabs.com.br>
2020-09-04 11:52:49 -0300

    feat: fixing table

--- Richard, The Compiler/src/app/screens/CompilerScreen/CompilerScreen.js
+++ Richard, The Compiler/src/app/screens/CompilerScreen/CompilerScreen.js
@@ -1,8 +1,9 @@
 /* eslint-disable no-undef */
+/* eslint-disable no-alert */
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from 'react';
-import * as CommandsCompiler from '../../helpers/comands';
+import * as CommandsCompiler from '../../helpers/commands';
 import './styles.scss';
 
 function CompilerScreen() {
@@ -16,31 +17,66 @@ function CompilerScreen() {
 
   const [rows, setRows] = useState([]);
 
-  function handleFileSelector(event) {
+  function loadTable(reader) {
     const tableRows = [];
-    const reader = new FileReader();
-    if (reader) {
-      reader.onload = async (e) => {
-        const fileRows = e.target.result.split('\n');
-        for (let i = 0; i < fileRows.length; i += 1) {
-          if (fileRows[i].trim() !== '') {
-            const rowValues = fileRows[i].split(' ');
+    reader.onload = async (e) => {
+      const fileRows = e.target.result.split('\n');
+      for (let i = 0; i < fileRows.length; i += 1) {
+        if (fileRows[i].trim() !== '') {
+          if (fileRows[i].indexOf('//') > 0) {
+            const divideComments = fileRows[i].split('//');
+            const comments = divideComments[1];
+            const instruction = divideComments[0];
             const tableRowWithPadding = [];
-            for (let j = 0; j < headings.length; j += 1) {
-              if (j > rowValues.length) {
-                tableRowWithPadding[j] = '';
-              } else {
-                tableRowWithPadding[j] = rowValues[j];
-              }
+            let newVector = [];
+
+            const rowValues = instruction.split(' ');
+            rowValues.forEach((item) => {
+              if (item.includes(',')) {
+                newVector = newVector.concat(item.split(','));
+              } else newVector.push(item);
+            });
+
+            tableRowWithPadding[0] = i;
+            tableRowWithPadding[4] = comments;
+            for (let j = 0; j < headings.length - 2; j += 1) {
+              if (j > newVector.length) {
+                tableRowWithPadding[j + 1] = '';
+              } else tableRowWithPadding[j + 1] = newVector[j];
+            }
+            tableRows[i] = tableRowWithPadding;
+          } else {
+            const rowValues = fileRows[i].split(' ');
+            let newVector = [];
+            rowValues.forEach((item) => {
+              if (item.includes(',')) {
+                newVector = newVector.concat(item.split(','));
+              } else newVector.push(item);
+            });
+            const tableRowWithPadding = [];
+            tableRowWithPadding[0] = i;
+            for (let j = 0; j < headings.length - 1; j += 1) {
+              if (j > newVector.length) {
+                tableRowWithPadding[j + 1] = '';
+              } else tableRowWithPadding[j + 1] = newVector[j];
             }
             tableRows[i] = tableRowWithPadding;
           }
         }
-        setRows(tableRows);
-      };
-      reader.readAsText(event.target.files[0]);
-      event.target.value = '';
-    }
+      }
+      setRows(tableRows);
+    };
+  }
+
+  async function handleFileSelector(event) {
+    if (event.target.files[0].type === 'text/plain') {
+      const reader = new FileReader();
+      if (reader) {
+        loadTable(reader);
+        reader.readAsText(event.target.files[0]);
+        event.target.value = '';
+      }
+    } else alert('Este tipo de arquivo não é suportado!');
   }
 
   function renderTableHeader() {
@@ -61,7 +97,7 @@ function CompilerScreen() {
 
   function runCode() {
     rows.forEach((item, index) => {
-      if (index === 1) console.log(item)
+      if (index === 0) console.log(item);
       // CommandsCompiler.commands(item[1]);
     });
   }
@@ -75,6 +111,7 @@ function CompilerScreen() {
               type="file"
               name="Arquivo"
               id="file-button"
+              accept=".txt"
               onChange={handleFileSelector}
             />
             <label htmlFor="file-button">Arquivo</label>
